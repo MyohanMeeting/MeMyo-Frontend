a1762ccd6ef3ae61b92c5435acbd6e9d3c9ed530
mini <jma1020@naver.com>
2023-05-27 05:11:28 +0900

    Refactor: 게시판 Select 박스 컴포넌트화

--- src/pages/post/post.tsx
+++ src/pages/post/post.tsx
@@ -1,3 +1,5 @@
+import AdoptSelectBox from '../../components/adopt-post/AdoptSelectBox';
+
 function Post() {
   return (
     <div className="flex justify-center">
@@ -34,53 +36,12 @@ function Post() {
               </div>
             </div>
 
-            <button type="button" disabled className="relative w-full   mb-52">
-              <div className="relative w-full flex items-center justify-between border-2 border-memyo-yellow4 rounded-md px-2 py-1 ">
-                종류
-                {/* svg 는 컴포넌트로 쉽게 쓸 수 있게끔 변경예정  */}
-                <svg viewBox="0 0 512 512" width="1rem" height="1rem">
-                  <path
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="48"
-                    d="M112 184l144 144 144-144"
-                  />
-                </svg>
-              </div>
-              <ul
-                className="absolute block left-0 right-0 top-full  max-h-36 overflow-y-auto transition-all py-2 bg-slate-100 rounded-lg text-center shadow-md  "
-                // webkit scroll bar : none -- tailwind 는 따로 패키지를 다운받아야함.
-                // 해당 ul tag top:calc(100% + 0.5rem) 은 어떻게 적용을 해줘야할까?
-              >
-                <li
-                  className="relative w-full flex items-center justify-center p-3 transition-all cursor-pointer hover:bg-memyo-yellow1 hover:bg-opacity-50  "
-                  // tailwind rgba 적용 방법?
-                >
-                  <svg
-                    viewBox="0 0 16 16 "
-                    width="1rem"
-                    height="1rem"
-                    className="absolute left-4 "
-                    // 해당 부분 Checkbox SVG hidden은 css 처리로 작업하기에 너무 깊어져서 상태를 이용해 해결 예정
-                  >
-                    <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z" />
-                  </svg>
-
-                  <span>러시안블루</span>
-                </li>
-                <li className="relative w-full flex items-center justify-center p-3 transition-all cursor-pointer hover:bg-memyo-yellow1 hover:bg-opacity-50  ">
-                  고양이
-                </li>
-                <li className="relative w-full flex items-center justify-center p-3 transition-all cursor-pointer hover:bg-memyo-yellow1 hover:bg-opacity-50  ">
-                  러시안블루
-                </li>
-                <li className="relative w-full flex items-center justify-center p-3 transition-all cursor-pointer hover:bg-memyo-yellow1 hover:bg-opacity-50  ">
-                  고양이
-                </li>
-              </ul>
-            </button>
+            <AdoptSelectBox
+              id={1}
+              label="종류"
+              options={['러시안블루', '고양이', '러시안블루']}
+              selectedValue="state 값"
+            />
           </div>
 
           <div className="flex-1 flex flex-col gap-6">
--- src/pages/post/postList.tsx
+++ src/pages/post/postList.tsx
@@ -1,4 +1,4 @@
-import SVGIcon from '../../components/shared/SVGIcon/SVGIcon';
+import AdoptSelectBox from '../../components/adopt-post/AdoptSelectBox';
 
 function PostList() {
   // 현재 https://www.behance.net/gallery/165319265/Pet-Scout-Animal-pet-search-web-service-UIUX-Design 피그마가 없어서 자세한 사이즈 조정을 못하여, 회의 지나고 조정 예정
@@ -35,54 +35,12 @@ function PostList() {
                 test
               </button>
             </div>
-
-            <button type="button" disabled className="relative w-full   mb-52">
-              <div className="relative w-full flex items-center justify-between border-2 border-memyo-yellow4 rounded-md px-2 py-1 ">
-                종류
-                {/* svg 는 컴포넌트로 쉽게 쓸 수 있게끔 변경예정  */}
-                <svg viewBox="0 0 512 512" width="1rem" height="1rem">
-                  <path
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="48"
-                    d="M112 184l144 144 144-144"
-                  />
-                </svg>
-              </div>
-              <ul
-                className="absolute block left-0 right-0 top-full  max-h-36 overflow-y-auto transition-all py-2 bg-slate-100 rounded-lg text-center shadow-md  "
-                // webkit scroll bar : none -- tailwind 는 따로 패키지를 다운받아야함.
-                // 해당 ul tag top:calc(100% + 0.5rem) 은 어떻게 적용을 해줘야할까?
-              >
-                <li
-                  className="relative w-full flex items-center justify-center p-3 transition-all cursor-pointer hover:bg-memyo-yellow1 hover:bg-opacity-50  "
-                  // tailwind rgba 적용 방법?
-                >
-                  <svg
-                    viewBox="0 0 16 16 "
-                    width="1rem"
-                    height="1rem"
-                    className="absolute left-4 "
-                    // 해당 부분 Checkbox SVG hidden은 css 처리로 작업하기에 너무 깊어져서 상태를 이용해 해결 예정
-                  >
-                    <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z" />
-                  </svg>
-                  {/* <SVGIcon icon="CheronDownIcon" /> */}
-                  <span>러시안블루</span>
-                </li>
-                <li className="relative w-full flex items-center justify-center p-3 transition-all cursor-pointer hover:bg-memyo-yellow1 hover:bg-opacity-50  ">
-                  고양이
-                </li>
-                <li className="relative w-full flex items-center justify-center p-3 transition-all cursor-pointer hover:bg-memyo-yellow1 hover:bg-opacity-50  ">
-                  러시안블루
-                </li>
-                <li className="relative w-full flex items-center justify-center p-3 transition-all cursor-pointer hover:bg-memyo-yellow1 hover:bg-opacity-50  ">
-                  고양이
-                </li>
-              </ul>
-            </button>
+            <AdoptSelectBox
+              id={1}
+              label="종류"
+              options={['러시안블루', '고양이', '러시안블루']}
+              selectedValue="state 값"
+            />
 
             <div className="flex justify-between gap-5   ">
               <button className=" py-2  flex-1 border-2 border-memyo-yellow1  text-center  rounded-lg">
