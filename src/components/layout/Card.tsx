994d23652e0dfd52cc4f6b76224a3b1f4b837e8a
vanillovin <vanillovin@gmail.com>
2023-05-27 20:32:22 +0900

    Design: Card 스타일 변경, svg 추가

--- src/components/layout/Card.tsx
+++ src/components/layout/Card.tsx
@@ -3,15 +3,33 @@ const tempImgUrl =
 
 function Card() {
   return (
-    <div className="border border-memyo-yellow2 hover:bg-memyo-yellow2 rounded-md transition-all cursor-pointer hover:p-2">
+    <div
+      className="border border-memyo-yellow4 hover:bg-memyo-yellow2 rounded-md transition-all 
+      cursor-pointer hover:p-2 shadow-sm hover:shadow-md hover:border-memyo-yellow2 box-border"
+    >
       <img
-        className="border-b border-memyo-yellow2 rounded-t-md hover:rounded-md"
+        className="border-b border-memyo-yellow4 rounded-t-md hover:rounded-md"
         src={tempImgUrl}
       />
       <div className="p-2">
         <div className="flex items-center justify-between mb-2">
           <span className="font-semibold">루루</span>
-          <button className="">♡</button>
+          <button className="">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-7 h-7 text-memyo-yellow4 hover:fill-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+              />
+            </svg>
+          </button>
         </div>
         <div className="flex items-center justify-between">
           <span className="text-sm font-semibold">묘종</span>
