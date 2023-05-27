a54816d98c7a4310c6c736b669b9cf7f2e9e62bf
정민킴 <45344418+jma1020@users.noreply.github.com>
2023-05-27 20:56:41 +0900

    Design: 입양 페이지 UI 구성 (#3)
    * Design: 입양 페이지 UI 구성
    
    * Refactor: 게시판 Select 박스 컴포넌트화
    
    * Refactor: 사진 Input 컴포넌트 화

---
+++ src/pages/post/postItem.tsx
@@ -0,0 +1,11 @@
+function PostItem() {
+  return (
+    <div className="flex justify-center">
+      <main className="w-full max-w-5xl mt-24">
+        <p>Back</p>
+      </main>
+    </div>
+  );
+}
+
+export default PostItem;
