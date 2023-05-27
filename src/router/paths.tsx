b6d4496bfe8907a6817a463f439b61d95203af96
wecaners <wecaners@naver.com>
2023-05-27 21:18:46 +0900

    Chore: router 수정

--- src/router/paths.tsx
+++ src/router/paths.tsx
@@ -1,10 +1,7 @@
 import { RouteObject } from 'react-router-dom';
 import NavbarWrapper from '../components/layout/NavbarWrapper';
-<<<<<<< HEAD
 import MyPage from '../pages/mypage/MyPage';
-=======
 import LoginPage from '../pages/login/LoginPage';
->>>>>>> develop
 import SignupPage from '../pages/signup/SignupPage';
 
 const paths: RouteObject[] = [
