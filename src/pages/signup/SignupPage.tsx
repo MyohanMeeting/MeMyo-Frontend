604b32713ce9084ebfd0438c3299083dcf9e2700
wecaners <wecaners@naver.com>
2023-05-31 17:02:09 +0900

    Design: 로그인 인풋창 배경 투명도 수정

--- src/pages/signup/SignupPage.tsx
+++ src/pages/signup/SignupPage.tsx
@@ -12,38 +12,42 @@ function SignupPage() {
       }}
       className="relative flex items-center justify-center max-w-5xl min-h-screen md:p-16"
     >
-      <div className="absolute flex w-4/5 shadow-2xl bottom-12 bg-memyo-yellow8 rounded-2xl">
+      <div className="absolute flex w-4/5 bg-opacity-50 border border-black shadow-2xl bottom-12 bg-memyo-yellow8 rounded-2xl">
         <div className="flex flex-col w-7/12 m-auto">
           <div className="text-center md:mt-8">
-            <h1 className="hidden text-4xl font-semibold text-white md:block">묘한만남</h1>
-            <h3 className="hidden text-gray-600 md:block text-md">Adopt your Life partner</h3>
+            <h1 className="hidden text-4xl font-semibold text-white shadow-inner md:block">
+              묘한만남
+            </h1>
+            <h3 className="hidden text-gray-600 md:block text-md">Adopt your Life Partner</h3>
           </div>
           <div className="space-y-6 md:ml-10 md:mr-10">
             <div className="space-y-3 text-center">
               <input
                 type="text"
-                className="w-full h-10 mt-12 rounded-xl indent-3 focus:outline-none"
+                className="w-full h-10 mt-12 shadow-2xl rounded-xl indent-3 focus:outline-none"
                 placeholder="이메일"
               />
 
               <input
                 type="password"
-                className="w-full h-10 rounded-xl indent-3 focus:outline-none"
+                className="w-full h-10 shadow-2xl rounded-xl indent-3 focus:outline-none"
                 placeholder="비밀번호"
               />
               <input
                 type="text"
-                className="w-full h-10 mt-12 rounded-xl indent-3 focus:outline-none"
+                className="w-full h-10 mt-12 shadow-2xl rounded-xl indent-3 focus:outline-none"
                 placeholder="닉네임"
               />
               <input
                 type="text"
-                className="w-full h-10 mt-12 rounded-xl indent-3 focus:outline-none"
+                className="w-full h-10 mt-12 shadow-2xl rounded-xl indent-3 focus:outline-none"
                 placeholder="전화번호"
               />
             </div>
             <div className="flex items-center justify-center">
-              <button className="w-full h-10 text-white bg-blue-500 rounded-xl">회원가입</button>
+              <button className="w-full h-10 text-white bg-blue-500 shadow-2xl rounded-xl">
+                회원가입
+              </button>
             </div>
             <div className="flex items-center justify-center pb-4 space-x-4">
               <p className="text-xs">이미 회원이신가요? </p>
