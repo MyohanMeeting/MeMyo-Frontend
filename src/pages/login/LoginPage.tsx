413e11d14f63855a8c355276251710bd8aaa8404
wecaners <wecaners@naver.com>
2023-05-31 13:22:29 +0900

    Design: 로그인 페이지 영단어 -> 한글 수정

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -11,12 +11,12 @@ function LoginPage() {
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
       }}
-      className="relative flex items-center justify-center max-w-5xl min-h-screen md:p-16 rounded-2xl"
+      className="relative flex items-center justify-center max-w-5xl min-h-screen md:p-16"
     >
       <div className="absolute flex w-4/5 shadow-2xl bottom-12 bg-memyo-yellow8 rounded-2xl">
         <div className="flex flex-col w-7/12 m-auto">
           <div className="mt-8 text-center">
-            <h1 className="text-4xl font-semibold text-white">MeMyo</h1>
+            <h1 className="text-4xl font-semibold text-white">묘한만남</h1>
             <h3 className="text-gray-600 text-md">Adopt your Life partner</h3>
           </div>
           <div className="space-y-6">
@@ -24,18 +24,17 @@ function LoginPage() {
               <input
                 type="text"
                 className="w-full h-10 mt-12 rounded-xl indent-3 focus:outline-none"
-                placeholder="E-mail"
+                placeholder="이메일"
               />
 
               <input
                 type="password"
                 className="w-full h-10 rounded-xl indent-3 focus:outline-none"
-                placeholder="Password"
+                placeholder="비밀번호"
               />
             </div>
-
             <div className="space-y-1 text-center md:ml-10 md:mr-10">
-              <button className="w-full h-10 text-white bg-blue-500 rounded-xl">SignIn</button>
+              <button className="w-full h-10 text-white bg-blue-500 rounded-xl">로그인</button>
               <div className="pb-6">
                 <img
                   className="object-fill w-screen h-10 rounded-xl"
@@ -45,7 +44,7 @@ function LoginPage() {
               </div>
               <div className="flex items-center justify-center p-2 space-x-3">
                 <p className="text-xs text-gray-600">아직 아이디가 없으신가요?</p>
-                <p className="text-xs font-semibold">signup</p>
+                <p className="text-xs font-semibold">회원가입</p>
               </div>
             </div>
           </div>
