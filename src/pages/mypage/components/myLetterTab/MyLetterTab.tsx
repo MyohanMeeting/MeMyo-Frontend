3847ce57ce45e82f9cd13f8247a81acaf6834f8a
wecaners <wecaners@naver.com>
2023-06-01 19:14:24 +0900

    Design: 마이페이지 입양 관리 탭 데스크탑 뷰 완료

--- src/pages/mypage/components/myLetterTab/MyLetterTab.tsx
+++ src/pages/mypage/components/myLetterTab/MyLetterTab.tsx
@@ -2,7 +2,7 @@ import CatInfoCard from '../CatInfoCard';
 
 function MyLetterTab() {
   return (
-    <article className="max-w-lg pt-4 m-auto space-y-4 md:space-y-0 md:gap-y-6 md:grid-cols-3 md:grid md:m-auto">
+    <article className="max-w-lg pt-4 m-auto space-y-4 md:space-y-0 md:grid md:grid-cols-2 md:gap-4 md:max-w-none md:place-items-center">
       <CatInfoCard />
       <CatInfoCard />
       <CatInfoCard />
