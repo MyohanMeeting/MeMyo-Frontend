ac4c70967adad0b6db0fcbab5969dfced1a6e3f9
wecaners <wecaners@naver.com>
2023-06-01 17:37:31 +0900

    Design: 최애 친구 탭 데스크탑 뷰 레이아웃 수정

--- src/pages/mypage/components/favoriteTab/FavoriteTab.tsx
+++ src/pages/mypage/components/favoriteTab/FavoriteTab.tsx
@@ -3,11 +3,7 @@ import CatInfoCard from '../CatInfoCard';
 function FavoriteTab() {
   return (
     <div>
-      <section className="max-w-lg pt-4 m-auto space-y-4 md:space-y-0 md:gap-y-6 md:grid-cols-3 md:grid md:m-auto">
-        <CatInfoCard />
-        <CatInfoCard />
-        <CatInfoCard />
-        <CatInfoCard />
+      <section className="max-w-lg pt-4 m-auto space-y-4 md:space-y-0 md:grid md:grid-cols-2 md:gap-4 md:max-w-none md:place-items-center">
         <CatInfoCard />
         <CatInfoCard />
         <CatInfoCard />
