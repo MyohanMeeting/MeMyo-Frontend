be74dc9bc5b06b59be4ac38b7833ddb0f36e654c
woody <90304025+wecaners@users.noreply.github.com>
2023-06-28 12:31:02 +0900

    Design: 회원가입 페이지 디자인 수정 (#8)
    * Design: 회원가입 페이지 로그인 페이지와 컨셉 비슷하게 수정
    
    * Design: 회원가입 폰트 색상 검정 -> 흰색 변경
    
    * Design: 로그인 인풋창 배경 투명도 수정
    
    * Chore: assets signup 폴더 생성 -> signup 메인 이미지 저장
    
    * Design: 회원가입 인풋창 테두리 제거

--- src/pages/signup/SignupPage.tsx
+++ src/pages/signup/SignupPage.tsx
@@ -1,55 +1,63 @@
-import signupMainImg from '../../assets/signupMainImg.jpg';
+import signupMainImg from '../../assets/signup/signupMainImg.jpg';
+import signupBgImg from '../../assets/signup/signup-bg-img.jpeg';
 
 function SignupPage() {
   return (
-    <div className="flex items-center justify-center min-h-screen mx-8 max-w-7xl md:p-16">
-      <div className="flex bg-yellow-300 shadow-2xl rounded-2xl">
-        <div className="flex flex-col w-7/12 m-auto text-center">
-          <div className="pt-8 md:p-7">
-            <h1 className="text-4xl font-semibold">Signup</h1>
-            <h3 className="text-md"></h3>
+    <div
+      style={{
+        backgroundImage: `url(${signupBgImg})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }}
+      className="relative flex items-center justify-center max-w-5xl min-h-screen md:p-16"
+    >
+      <div className="absolute flex w-4/5 bg-opacity-50 shadow-2xl bottom-12 bg-memyo-yellow8 rounded-2xl">
+        <div className="flex flex-col w-7/12 m-auto">
+          <div className="text-center md:mt-8">
+            <h1 className="hidden text-4xl font-semibold text-white shadow-inner md:block">
+              묘한만남
+            </h1>
+            <h3 className="hidden text-gray-600 md:block text-md">Adopt your Life Partner</h3>
           </div>
-          <div className="space-y-8 text-center md:ml-10 md:mr-10 ">
-            <input
-              type="text"
-              className="w-full h-10 mt-12 rounded-xl indent-3 focus:outline-none"
-              placeholder="E-mail"
-            />
+          <div className="space-y-6 md:ml-10 md:mr-10">
+            <div className="space-y-3 text-center">
+              <input
+                type="text"
+                className="w-full h-10 mt-12 shadow-2xl rounded-xl indent-3 focus:outline-none"
+                placeholder="이메일"
+              />
 
-            <input
-              type="password"
-              className="w-full h-10 rounded-xl indent-3 focus:outline-none"
-              placeholder="Password"
-            />
-
-            <input
-              type="text"
-              className="w-full h-10 rounded-xl indent-3 focus:outline-none"
-              placeholder="Nickname"
-            />
-
-            <input
-              type="text"
-              className="w-full h-10 rounded-xl indent-3 focus:outline-none"
-              placeholder="PhoneNumber"
-            />
-
-            <div>
-              <label className="p-2 bg-yellow-200 rounded-xl" htmlFor="input-file">
-                Add Profile Photo
-              </label>
-              <input type="file" id="input-file" style={{ display: 'none' }} />
+              <input
+                type="password"
+                className="w-full h-10 shadow-2xl rounded-xl indent-3 focus:outline-none"
+                placeholder="비밀번호"
+              />
+              <input
+                type="text"
+                className="w-full h-10 mt-12 shadow-2xl rounded-xl indent-3 focus:outline-none"
+                placeholder="닉네임"
+              />
+              <input
+                type="text"
+                className="w-full h-10 mt-12 shadow-2xl rounded-xl indent-3 focus:outline-none"
+                placeholder="전화번호"
+              />
             </div>
-            <div className="pb-6">
-              <button className="w-full h-10 text-white rounded-xl bg-signup-signupBlue">
-                Signup
+            <div className="flex items-center justify-center">
+              <button className="w-full h-10 text-white bg-blue-500 shadow-2xl rounded-xl">
+                회원가입
               </button>
+            </div>
+            <div className="flex items-center justify-center pb-4 space-x-4">
+              <p className="text-xs">이미 회원이신가요? </p>
+              <p className="text-xs font-semibold">로그인</p>
             </div>
           </div>
         </div>
         <div className="hidden w-9/12 md:block">
           <img
-            className="object-cover w-full h-full rounded-r-2xl"
+            className="object-fill w-full h-full rounded-r-2xl"
             src={signupMainImg}
             alt="signupMainImage"
           />
