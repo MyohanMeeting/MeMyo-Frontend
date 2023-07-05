28fef184720f7c235f86bb1ac91f31c0be790454
wecaners <wecaners@naver.com>
2023-07-05 14:51:20 +0900

    Feat: Nav 왼쪽아이콘 home 아이콘으로 변경 및 라우팅

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -24,7 +24,7 @@ function NavBar() {
   return (
     <>
       <nav className="flex items-center justify-between py-6 md:hidden">
-        <button onClick={() => navigate(-1)}>
+        <Link to="/">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -33,12 +33,15 @@ function NavBar() {
             stroke="currentColor"
             className="w-12 h-8 text-memyo-yellow8"
           >
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+            />
           </svg>
-        </button>
-        <Link to="/">
-          <h1 className="text-3xl font-semibold text-memyo-yellow9">{pathName}</h1>
         </Link>
+        <h1 className="text-3xl font-semibold text-memyo-yellow9">{pathName}</h1>
+
         <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
       </nav>
       <div className="hidden md:block">
