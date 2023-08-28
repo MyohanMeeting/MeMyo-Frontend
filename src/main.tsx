f41d1d5dd8117651c6afd5cdeb0980a53748d644
wecaners <wecaners@naver.com>
2023-08-28 17:03:38 +0900

    Remove: Approutes.tsx 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
-<<<<<<< HEAD
-import AppRoutes from './router/AppRoutes';
-import { store } from './redux/configureStore';
-=======
 import store from '@redux/store'
-import router from '@/router';
+import { RouterProvider } from 'react-router';
+import router from './router';
 
 
->>>>>>> develop
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <AppRoutes />
+      <RouterProvider router={router}/>
     </Provider>
   </React.StrictMode>
 );
