27bd9e60af7c8ead6e154f949c04b519796da527
heywon0909 <hw7319@naver.com>
2023-09-02 12:26:45 +0900

    vite 셋팅 원복

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: '/MeMyo-Frontend/',
   plugins: [react()],
   server: {
     proxy: {
