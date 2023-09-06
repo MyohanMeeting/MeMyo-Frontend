99e39e353e44faeb8c752609e411edd24c21455e
wecaners <wecaners@naver.com>
2023-09-06 17:54:23 +0900

    Fix: build 에러 수정

--- src/pages/mypage/components/favoriteTab/FavoriteTab.tsx
+++ src/pages/mypage/components/favoriteTab/FavoriteTab.tsx
@@ -4,12 +4,11 @@ import { useSelector } from 'react-redux';
 import { RootState } from '@redux/store';
 import { useEffect } from 'react';
 import { getFavoriteFriendThunk } from '@redux/thunks/FavoriteFriendThunk';
+import { FavoriteFriend } from '../../../../types/FavoriteFriend';
 
 function FavoriteTab() {
   const dispatch = useThunkDispatch();
-  const favoriteFriend = useSelector(
-    (state: RootState) => state.persistedReducer.favoriteFriend.favoriteFriends
-  );
+  const favoriteFriend = useSelector((state: RootState) => state.favoriteFriend.favoriteFriends);
 
   useEffect(() => {
     dispatch(getFavoriteFriendThunk());
@@ -18,7 +17,7 @@ function FavoriteTab() {
   return (
     <div>
       <section className="max-w-lg max-h-screen pt-4 m-auto space-y-4 overflow-auto md:space-y-0 md:grid md:grid-cols-2 md:gap-4 md:max-w-none md:place-items-center scrollbar-hide">
-        {favoriteFriend.map((item) => {
+        {favoriteFriend.map((item: FavoriteFriend) => {
           return (
             <div>
               <FavoriteFriendCard info={item} />
