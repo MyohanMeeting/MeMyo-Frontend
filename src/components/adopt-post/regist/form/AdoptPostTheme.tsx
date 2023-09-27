917df674d7176925225968e2e29ccc0d568d0ba5
hyewon_park2 <hyewon_park2@tmax.co.kr>
2023-09-27 18:00:46 +0900

    입양 공고 신청 validation 체크

--- src/components/adopt-post/regist/form/AdoptPostTheme.tsx
+++ src/components/adopt-post/regist/form/AdoptPostTheme.tsx
@@ -1,40 +1,59 @@
-import {  memo, useCallback, useMemo } from 'react';
+import { memo, useCallback, useMemo } from 'react';
 import { useAppDispatch } from '@redux/hooks';
 import { setAdoptForm } from '@redux/slice/adoptSlice';
-interface Props{
-    title: string;
-    content: string;
+interface Props {
+  title: string;
+  content: string;
 }
 function AdoptPostTheme({ title, content }: Props) {
-    const dispatch = useAppDispatch();
-  
-    const registTitle = useMemo(() => title || '', [title]);
-    const registContent = useMemo(() => content || '', [content]);
-   
-    
-    const handleTheme = useCallback(<T extends HTMLInputElement | HTMLTextAreaElement>(e: React.ChangeEvent<T>) => {
-        const { name, value } = e.target;
-        console.log('name',name,value)
-        if (value == null) return;
-        const realName = name;
-        dispatch(setAdoptForm({[realName]: value}))
-    },[]);
-    
+  const dispatch = useAppDispatch();
 
-    return (
-        <>
-            <div className='flex mt-10 w-full'>
-                  <h2 className='text-lg font-bold font-pretendard'>유기묘 공고를 등록합니다</h2>
-              </div>
-              <div className='p-2 mt-5'>
-                  <h6 className='text-base font-bold'>등록한 유기묘 공고의 타이틀과 설명을 입력해주세요</h6>
-                    <label htmlFor="title" className="block mt-2 py-2 text-gray-700 w-full mb-2">타이틀</label>
-                    <input id="title" name="title" className="border-b appearance-none bg-transparent w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" defaultValue={registTitle} onChange={handleTheme}/>
-                    <label htmlFor="content" className="block mt-2 py-2 text-gray-700">설명</label>
-                    <textarea id="content" name="content" rows={4} className="h-full resize-none block p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" defaultValue={registContent} onChange={handleTheme} ></textarea>
-            </div>
-        </>
-            );
+  const registTitle = useMemo(() => title || '', [title]);
+  const registContent = useMemo(() => content || '', [content]);
+
+  const handleTheme = useCallback(
+    <T extends HTMLInputElement | HTMLTextAreaElement>(e: React.ChangeEvent<T>) => {
+      const { name, value } = e.target;
+      console.log('name', name, value);
+      if (value == null) return;
+      const realName = name;
+      dispatch(setAdoptForm({ [realName]: value }));
+    },
+    [dispatch]
+  );
+
+  return (
+    <>
+      <div className="flex mt-10 w-full">
+        <h2 className="text-lg font-bold font-pretendard">유기묘 공고를 등록합니다</h2>
+      </div>
+      <div className="p-2 mt-5">
+        <h6 className="text-base font-bold">등록한 유기묘 공고의 타이틀과 설명을 입력해주세요</h6>
+        <label htmlFor="title" className="block mt-2 py-2 text-gray-700 w-full mb-2">
+          타이틀
+        </label>
+        <input
+          id="title"
+          name="title"
+          className="border-b appearance-none bg-transparent w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+          type="text"
+          defaultValue={registTitle}
+          onChange={handleTheme}
+        />
+        <label htmlFor="content" className="block mt-2 py-2 text-gray-700">
+          설명
+        </label>
+        <textarea
+          id="content"
+          name="content"
+          rows={4}
+          className="h-full resize-none block p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          defaultValue={registContent}
+          onChange={handleTheme}
+        ></textarea>
+      </div>
+    </>
+  );
 }
 
 export default memo(AdoptPostTheme);
--- src/components/adopt-post/regist/mobile/AdoptRegistNextBut.tsx
+++ src/components/adopt-post/regist/mobile/AdoptRegistNextBut.tsx
@@ -2,7 +2,7 @@ import { AdoptPostContext } from '@/context/AdoptFormContext';
 import { useAppDispatch, useAppSelector, useThunkDispatch } from '@redux/hooks';
 import { setAdoptForm } from '@redux/slice/adoptSlice';
 import { setAdoptPostImg, setAdoptPostThunk } from '@redux/thunks/AdoptThunk';
-import { AdoptForm, catPicture } from '@/types/Adopt';
+import { AdoptForm, AdoptFormRequired, catPicture } from '@/types/Adopt';
 import { useCallback, useContext } from 'react';
 
 function AdoptRegistNextBut() {
@@ -13,18 +13,35 @@ function AdoptRegistNextBut() {
   const appDispatch = useAppDispatch();
   const thunkDispatch = useThunkDispatch();
 
+  type Form = {
+    [key in keyof Omit<AdoptForm, 'catPictures' | 'thumbnailId'>]: string;
+  };
+
+  let AdoptFormRequestForm: Form = {
+    title: '타이틀',
+    content: '설명',
+    cat: '유기묘 정보',
+    shelter: '입양기관 정보',
+  };
+
   const handleRegist = useCallback(async () => {
-    console.log('adopt', adoptPreviewImg);
+    if (adoptPreviewImg != null && adoptPreviewImg.length === 0)
+      return alert('고양이 사진을 업로드 해주세요');
+    for (const [key, value] of Object.entries(AdoptFormRequestForm)) {
+      const existKey = key as keyof AdoptForm;
+      if (adoptForm[existKey] == null) {
+        return alert(`${value}을 입력해주세요`);
+      }
+    }
+
     await thunkDispatch(setAdoptPostImg(adoptPreviewImg as File[])).then(async (result) => {
       if (result) {
-        console.log('result', result);
-
         const uploadId = result.payload as catPicture[];
-        console.log('uploadID', uploadId);
-        console.log('obj', { thumbnailId: uploadId[0], catPictures: uploadId });
+
         await appDispatch(setAdoptForm({ thumbnailId: uploadId[0], catPictures: uploadId }));
 
         console.log('adoptForm 이거는 ', adoptForm);
+
         if (adoptForm as Required<AdoptForm>) {
           console.log('adoptForm 완성되었니', adoptForm);
           thunkDispatch(setAdoptPostThunk(adoptForm as Required<AdoptForm>));
